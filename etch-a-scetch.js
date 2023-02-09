9b9a0d997f680693b767a7815531602c90bbac0a
Vadim <vadimshdrn@gmail.com>
2023-02-09 19:41:41 +0600

    Add function for create grid

--- etch-a-scetch.js
+++ etch-a-scetch.js
@@ -1,1 +1,10 @@
 const container = document.querySelector('.container');
+
+function createGrid(divs) {
+
+    for (let i = 0; i < divs; i++) {
+        let cells = document.createElement('div');
+        container.appendChild(cells);
+    }
+}
+createGrid(256);
