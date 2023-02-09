67140dff8b27cbfe5300f54eeb056cbd2a9d7099
Vadim <vadimshdrn@gmail.com>
2023-02-09 22:16:56 +0600

    case event mouseover colored div

--- etch-a-scetch.js
+++ etch-a-scetch.js
@@ -5,10 +5,14 @@ function createGrid(divs) {
     for (let i = 0; i < divs; i++) {
         let cells = document.createElement('div');
         container.appendChild(cells);
+        cells.classList.add('.grid');
     }
 }
 createGrid(256);
 
-container.addEventListener('mousemove', function (event) {
-    console.log(event.target);
+container.addEventListener('mouseover', function (event) {
+    let elem = event.target;
+    console.log(elem);
+    elem.style.backgroundColor = 'black';
     });
+
