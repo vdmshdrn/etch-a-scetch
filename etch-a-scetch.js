ac08e32a369f7ff9fbbd7f592f89496ec4f4ce95
Vadim <vadimshdrn@gmail.com>
2023-02-12 00:22:01 +0600

    Now grid styles are generated interactively

--- etch-a-scetch.js
+++ etch-a-scetch.js
@@ -4,9 +4,11 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
-function createGrid(divs) {
+function createGrid(size) {
 
-    for (let i = 0; i < divs; i++) {
+    for (let i = 0; i < size*size; i++) {
+        container.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows =`repeat(${size}, 1fr)`;
         let cells = document.createElement('div');
         cells.classList.add('grid');
         container.appendChild(cells);
@@ -14,7 +16,7 @@ function createGrid(divs) {
         cells.addEventListener('mousedown', coloredDiv);
     }
 }
-createGrid(256);
+createGrid(16);
 
 
 function coloredDiv(elem) {
