84902b735e2f9eff0ec520d74160d8e96c6537a0
Vadim <vadimshdrn@gmail.com>
2023-02-11 22:29:45 +0600

    Add coloredDiv function that works only wih a pressed lef mouse click

--- etch-a-scetch.js
+++ etch-a-scetch.js
@@ -1,18 +1,26 @@
 const container = document.querySelector('.container');
+const grid = document.querySelectorAll('.grid');
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function createGrid(divs) {
 
     for (let i = 0; i < divs; i++) {
         let cells = document.createElement('div');
+        cells.classList.add('grid');
         container.appendChild(cells);
-        cells.classList.add('.grid');
+        cells.addEventListener('mouseover', coloredDiv);
+        cells.addEventListener('mousedown', coloredDiv);
     }
 }
 createGrid(256);
 
-container.addEventListener('mouseover', function (event) {
-    let elem = event.target;
-    console.log(elem);
-    elem.style.backgroundColor = 'black';
-    });
+
+function coloredDiv(elem) {
+    if (elem.type === 'mouseover' && mouseDown) {
+    console.log(elem.target);
+    elem.target.style.backgroundColor = 'black';
+    }
+}
 
