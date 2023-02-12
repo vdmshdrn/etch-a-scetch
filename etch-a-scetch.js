64a09a90b77c1b7a010f64db60fb0dc970b460ec
Vadim <vadimshdrn@gmail.com>
2023-02-12 18:38:53 +0600

    Colors selected when the appropriate button is clicked

--- etch-a-scetch.js
+++ etch-a-scetch.js
@@ -1,14 +1,17 @@
+"use strict";
 const container = document.querySelector('.container');
 const grid = document.querySelectorAll('.grid');
+let color = 'black';
+
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 function createGrid(size) {
 
-    for (let i = 0; i < size*size; i++) {
-        container.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
-        container.style.gridTemplateRows =`repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++) {
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
         let cells = document.createElement('div');
         cells.classList.add('grid');
         container.appendChild(cells);
@@ -18,11 +21,18 @@ function createGrid(size) {
 }
 createGrid(16);
 
+function getColor(choiseColor) {
+
+    color = choiseColor;
+}
+
 
 function coloredDiv(elem) {
     if (elem.type === 'mouseover' && mouseDown) {
-    console.log(elem.target);
-    elem.target.style.backgroundColor = 'black';
+        if (color === 'random') {
+            elem.target.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            elem.target.style.backgroundColor = color;
+        }
     }
 }
-
